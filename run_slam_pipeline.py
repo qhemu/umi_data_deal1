edbfeb0f1d36a03c95e3d6bf9547a7a651dba2c9
hemu <qixiuxiuhemu@163.com>
2024-05-23 20:46:00 +0800

    Add files via upload

---
+++ run_slam_pipeline.py
@@ -0,0 +1,138 @@
+"""
+Main script for UMI SLAM pipeline.
+python run_slam_pipeline.py <session_dir>
+"""
+
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
+sys.path.append(ROOT_DIR)
+os.chdir(ROOT_DIR)
+# %%
+import pathlib
+import click
+import subprocess
+
+# %%
+@click.command()
+@click.argument('session_dir', nargs=-1)
+@click.option('-c', '--calibration_dir', type=str, default=None)
+def main(session_dir, calibration_dir):
+    # session_dir = "universal_manipulation_interface"
+    script_dir = pathlib.Path(__file__).parent.joinpath('scripts_slam_pipeline')  # PosixPath('/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface/scripts_slam_pipeline')
+    if calibration_dir is None:
+        calibration_dir = pathlib.Path(__file__).parent.joinpath('example', 'calibration')  # PosixPath('/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface/example/calibration')
+    else:
+        calibration_dir = pathlib.Path(calibration_dir)
+    assert calibration_dir.is_dir()
+    for session in session_dir:
+        session = pathlib.Path(os.path.expanduser(session)).absolute()  # PosixPath('/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface')
+
+        print("############## 00_process_videos #############")
+        script_path = script_dir.joinpath("00_process_videos.py")  # PosixPath('/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface/scripts_slam_pipeline/00_process_videos.py')
+        assert script_path.is_file()
+        cmd = [
+            'python', str(script_path),
+            str(session)
+        ]
+        '''
+        cmd:['python', '/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface/scripts_slam_pipeline/00_process_videos.py',
+        '/home/xiuxiu/PycharmProjects/mobilealoha/universal_manipulation_interface']
+        '''
+        print(cmd)
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+        print("############# 01_extract_gopro_imu ###########")
+        script_path = script_dir.joinpath("01_extract_gopro_imu.py")
+        assert script_path.is_file()
+        cmd = [
+            'python', str(script_path),
+            str(session)
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+        print("############# 02_create_map ###########")
+        script_path = script_dir.joinpath("02_create_map.py")
+        assert script_path.is_file()
+        demo_dir = session.joinpath('demos')
+        mapping_dir = demo_dir.joinpath('mapping')
+        assert mapping_dir.is_dir()
+        map_path = mapping_dir.joinpath('map_atlas.osa')
+        if not map_path.is_file():
+            cmd = [
+                'python', str(script_path),
+                '--input_dir', str(mapping_dir),
+                '--map_path', str(map_path)
+            ]
+            result = subprocess.run(cmd)
+            assert result.returncode == 0
+            assert map_path.is_file()
+
+        print("############# 03_batch_slam ###########")
+        script_path = script_dir.joinpath("03_batch_slam.py")
+        assert script_path.is_file()
+        cmd = [
+            'python', str(script_path),
+            '--input_dir', str(demo_dir),
+            '--map_path', str(map_path)
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+        print("############# 04_detect_aruco ###########")
+        script_path = script_dir.joinpath("04_detect_aruco.py")
+        assert script_path.is_file()
+        camera_intrinsics = calibration_dir.joinpath('gopro_intrinsics_2_7k.json')
+        aruco_config = calibration_dir.joinpath('aruco_config.yaml')
+        assert camera_intrinsics.is_file()
+        assert aruco_config.is_file()
+
+        cmd = [
+            'python', str(script_path),
+            '--input_dir', str(demo_dir),
+            '--camera_intrinsics', str(camera_intrinsics),
+            '--aruco_yaml', str(aruco_config)
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+        print("############# 05_run_calibrations ###########")
+        script_path = script_dir.joinpath("05_run_calibrations.py")
+        assert script_path.is_file()
+        cmd = [
+            'python', str(script_path),
+            str(session)
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+        print("############# 06_generate_dataset_plan ###########")
+        script_path = script_dir.joinpath("06_generate_dataset_plan.py")
+        assert script_path.is_file()
+        cmd = [
+            'python', str(script_path),
+            '--input', str(session)
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+
+## %%
+if __name__ == "__main__":
+    main()
+
+'''
+"umi_data_deal/degug_session"
+
+sudo apt-get update
+sudo apt install -y libosmesa6-dev libgl1-mesa-glx libglfw3 patchelf　（如果ｕnbuntu有系统问题，可以创建docker，这里偷懒了，没有用docker）
+conda env create -f conda_environment.yaml　（这一步如果太慢，就按作者的推荐来使用Miniforge）
+conda activate umi_data
+关于UMI SLAM
+进入下载的code文件中下载示例数据：
+wget --recursive --no-parent --no-host-directories --cut-dirs=2 --relative --reject="index.html*" https://real.stanford.edu/umi/data/example_demo_session/
+
+'''
